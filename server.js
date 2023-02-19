786d3ecebe153b28e8edf8de4caef0d9e8824e6e
Jacob Jeffries <jeffrij85@gmail.com>
2023-02-19 13:09:44 -0500

    Get route for homepage.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Global Constants
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const EXPRESS_PORT = process.env.PORT || 3012;
@@ -11,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 // Static delivery of the public folder HTML
 app.use(express.static('./public/'));
 
+// GET route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+):
 
 
 // Webserver listening on EXPRESS_PORT
