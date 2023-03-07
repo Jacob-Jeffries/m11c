65aa435f26f5cfd2ade18e091042073cd3228c76
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-07 14:14:18 -0500

    added GET /notes route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/api/notes', (req,res) =>
+app.get('/notes', (req,res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
